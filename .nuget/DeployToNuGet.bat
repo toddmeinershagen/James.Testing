8633b08011265572eead40e047e45e976d12a12e
unknown <tmeinershagen@D9QY4R1-TMEINER.medassets.com>
2014-06-27 12:46:01 -0500

    Modified the nuget packaging for the new assembly and for hiding the api key.

--- .nuget/DeployToNuGet.bat
+++ .nuget/DeployToNuGet.bat
@@ -1,7 +1,11 @@
 nuget.exe update -self
 ECHO Y | DEL *.nupkg
 
-#NOTE - Using an account for todd@meinershagen.net to publish to NuGet.org.  This can be changed in the future.
-nuget.exe setApiKey 565e6305-d5e8-41ec-a70e-494cbb0dc6cf
+REM Using an account for todd@meinershagen.net to publish to NuGet.org.  This can be changed in the future.
+set /p NuGetApiKey= Please enter the project's NuGet API Key:
+nuget.exe setApiKey %NuGetApiKey%
+
 nuget.exe pack ..\James.Testing\James.Testing.csproj
+nuget.exe pack ..\James.Abstractions.System\James.Abstractions.System.csproj
+
 nuget.exe push *.nupkg
