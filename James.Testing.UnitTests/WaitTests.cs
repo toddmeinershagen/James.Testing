c83f84226050b243f6654c14cb9c32a7a459b264
unknown <tmeinershagen@D9QY4R1-TMEINER.medassets.com>
2014-08-21 19:20:04 -0500

    Added Wait.For() and Wait.Until() methods to the James.Testing assembly.

---
+++ James.Testing.UnitTests/WaitTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace James.Testing.UnitTests
+{
+    [TestFixture]
+    public class WaitTests
+    {
+        [TestCase(0)]
+        [TestCase(250)]
+        [TestCase(500)]
+        public void given_positive_number_when_waiting_for_milliseconds_should_wait_for_seconds(int expected)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            Wait.For(expected).Milliseconds();
+
+            watch.Stop();
+            watch.Elapsed.Should().BeCloseTo(TimeSpan.FromMilliseconds(expected));
+        }
+
+        [TestCase(0)]
+        [TestCase(2)]
+        public void given_positive_number_when_waiting_for_seconds_should_wait_for_seconds(int expected)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            Wait.For(expected).Seconds();
+
+            watch.Stop();
+            watch.Elapsed.Should().BeCloseTo(TimeSpan.FromSeconds(expected));
+        }
+
+        [Test]
+        public void given_negative_number_when_waiting_for_seconds_should_throw()
+        {
+            Action action = () => Wait.For(-1).Seconds();
+            action.ShouldThrow<ArgumentOutOfRangeException>().WithMessage("Number must be non-negative.\r\nParameter name: numberOf");
+        }
+
+        [Test]
+        public void given_predicate_when_waiting_until_predicate_is_true_should_wait_until_true()
+        {
+            int counter = 0;
+            Wait.Until(() => ++counter == 5);
+
+            counter.Should().Be(5);
+        }
+
+        [Test]
+        public void given_predicate_never_returns_true_when_waiting_until_predicate_is_true_should_wait_until_default_timeout()
+        {
+            var defaultTimeout = TimeSpan.FromSeconds(15);
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            Wait.Until(() => false);
+
+            watch.Stop();
+            watch.Elapsed.Should().BeCloseTo(defaultTimeout);
+        }
+
+        [Test]
+        public void given_predicate_never_returns_true_and_timeout_when_waiting_until_predicate_is_true_should_wait_until_timeout()
+        {
+            var timeout = TimeSpan.FromMilliseconds(5);
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            Wait.Until(() => false, timeout);
+
+            watch.Stop();
+            watch.Elapsed.Should().BeCloseTo(timeout);
+        }
+
+        [Test]
+        public void given_null_predicate_and_timeout_when_waiting_until_predicate_is_true_should_wait_until_timeout()
+        {
+            var timeout = TimeSpan.FromMilliseconds(5);
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            Wait.Until(null, timeout);
+
+            watch.Stop();
+            watch.Elapsed.Should().BeCloseTo(timeout);
+        }
+    }
+}
---
+++ James.Testing/Wait.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace James.Testing
+{
+    public class Wait
+    {
+        public static IWaitFor For(int numberOf)
+        {
+            if (numberOf < 0)
+                throw new ArgumentOutOfRangeException("numberOf", "Number must be non-negative.");
+
+            return new WaitFor(numberOf);
+        }
+
+        public static void Until(Func<bool> predicate)
+        {
+            Until(predicate, TimeSpan.FromSeconds(15));
+        }
+
+        public static void Until(Func<bool> predicate, TimeSpan timeout)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            while ((predicate == null || predicate() == false) && watch.Elapsed < timeout)
+            {}
+        }
+
+        private class WaitFor : IWaitFor
+        {
+            private readonly int _numberOf;
+
+            public WaitFor(int numberOf)
+            {
+                _numberOf = numberOf;
+            }
+
+            public void Milliseconds()
+            {
+                Thread.Sleep(_numberOf);
+            }
+
+            public void Seconds()
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(_numberOf));
+            }
+        }
+    }
+
+    public interface IWaitFor
+    {
+        void Milliseconds();
+        void Seconds();
+    }
+}
